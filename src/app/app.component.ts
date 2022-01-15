2551d1dbb7bd86e9efdeef30b56d5852c875dfe8
magamex <magamex.22@gmail.com>
2022-01-15 20:09:55 -0300

    crear proyecto, agrego bulma, modifico el componente app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // Selector para utilizarlo en index.html
   selector: 'app-root',
+  // Template del HTML llamando el archivo tambien se puede usar template:``
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'basesAngular';
+  titulo:string = 'Contador App';
 }
