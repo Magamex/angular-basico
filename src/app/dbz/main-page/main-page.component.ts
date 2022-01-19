69b14d4becc00610cb54aed3954776957a947c47
magamex <magamex.22@gmail.com>
2022-01-18 23:35:33 -0300

    agregan componentes y modulo dbz

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Personaje{
+  nombre: string;
+  poder: number;
+}
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.scss']
+})
+export class MainPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  personajes: Personaje[] = [
+    {nombre: 'Goku', poder: 9000},
+    {nombre: 'Vegeta', poder: 8000},
+  ];
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  agregarPersonaje() {
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+  }
+
+}
